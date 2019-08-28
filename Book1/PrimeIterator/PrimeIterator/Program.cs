48320d9f6bdb7a54c1225cce2cf103710b4a83e0
ncandaffio <44885428+ncandaffio@users.noreply.github.com>
2019-08-28 07:42:05 -0400

    8/28

---
+++ Book1/PrimeIterator/PrimeIterator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace PrimeIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimeNumbers pm = new PrimeNumbers();
+            foreach (int num in pm.PrimeTo(1000))
+            {
+                Console.WriteLine(num);
+            }
+
+            // Wait for user to acknowledge the results
+            Console.WriteLine("Press any key to terminate...");
+            Console.Read();
+        }
+
+            class PrimeNumbers
+            {
+                public System.Collections.IEnumerable PrimeTo(int maximum)
+                {
+                yield return 1;
+                for (int i = 2; i < maximum; i++)
+                {
+                    bool isPrime = true;
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime)
+                    {
+                        yield return i;
+                    }
+                }
+                }
+            }
+        
+    }
+}
