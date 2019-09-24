95a154183e44fd731bed75d9eaf703e53297afab
Nick Candaffio <ncandaffio@gmail.com>
2019-09-24 06:55:27 -0400

    sampe added

---
+++ Book2/SampleProgress/SampleProgress/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SampleProgress
+{
+    partial class ProgressBarForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(122, 81);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(472, 51);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(122, 231);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(190, 63);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(404, 231);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(190, 63);
+            this.buttonClose.TabIndex = 2;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.ButtonClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(291, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Progress";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Visible = false;
+            // 
+            // ProgressBarForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 398);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "ProgressBarForm";
+            this.Text = "Progress Bar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonClose;
+        public System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Book2/SampleProgress/SampleProgress/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleProgress
+{
+    public partial class ProgressBarForm : Form
+    {
+
+        //Declare Delegates
+        delegate void UpdateProgressCallback(string label);
+
+        private void DoSomethingLengthy(UpdateProgressCallback updateProgress)
+        {
+            int duration = 2000;
+            int updateInterval = duration / 10;
+
+            for (int i = 0; i < duration; i++)
+            {
+                double summation = 1;
+                Console.WriteLine("Step {0}", i);
+                for (int j = 0; j < i; j++)
+                {
+                    summation += Math.Pow(summation, Math.Sqrt(j));
+                }
+
+                //Update every 10th
+                if (i % 10 == 0 && updateProgress != null)
+                {
+                    Console.WriteLine("Update progress");
+                    updateProgress("Step " + i + " Complete");
+                }
+                
+            }
+        }
+
+        public ProgressBarForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label1.Refresh();
+            UpdateProgressCallback callback = new UpdateProgressCallback(this.DoUpdate);
+            DoSomethingLengthy(callback);
+            progressBar1.Value = 0;
+            label1.Visible = false;
+            label1.Refresh();
+        }
+
+        private void DoUpdate(string label)
+        {
+            label1.Text = label;
+            label1.Refresh();
+            progressBar1.PerformStep();
+        }
+
+    }
+}
